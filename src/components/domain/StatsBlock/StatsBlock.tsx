0af3e5fee41c7523993813ccbcacc59c0d12a3e8
dia-avv <eaa24avv@students.eaaa.dk>
2026-02-07 11:54:16 +0100

    dynamically fetch events number

--- src/components/domain/StatsBlock/StatsBlock.tsx
+++ src/components/domain/StatsBlock/StatsBlock.tsx
@@ -1,12 +1,15 @@
 import CountUpCard from "./_Nested/CountUpCard";
 import styles from "./StatsBlock.module.css";
+import { pastEvents } from "../../../content/events";
 
 export default function StatsBlock() {
+  const eventsCount = pastEvents.length;
+
   return (
     <div className={styles.statsBlock}>
       <h1>Past Events</h1>
       <div className={styles.statsCards}>
-        <CountUpCard amount={11} subtitle="Events" />
+        <CountUpCard amount={eventsCount} subtitle="Events" />
         <CountUpCard amount={9} subtitle="Collaborations" />
         <CountUpCard amount={400} subtitle="Participants" suffix="+" />
       </div>
